046b81c2a84a610e248a7422fd36d7968cc472c2
adeke910 <adekemibamiteko@gmail.com>
2026-05-07 14:45:07 +0100

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Sample-NFT-Project/",
+  base: "./",
   plugins: [svgr(), react(), tailwindcss()],
   resolve: {
     alias: {
